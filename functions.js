ddde33bf7a062924d0d2a9a749430d0348911fd2
Daichi117 <147604848+Daichi117@users.noreply.github.com>
2023-12-28 14:00:17 +0900

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,93 @@
+(function(){
+
+
+    'use strict';
+
+
+
+
+
+
+
+
+
+
+
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    
+    
+    
+    
+    hamburger.addEventListener( "click", mobileMenu);
+    
+    
+    function mobileMenu(){
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+       
+       
+        
+        
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+        
+       
+        
+        
+        
+       
+        
+    
+})();
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
